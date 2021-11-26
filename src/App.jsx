1aafb6920b67b9cd4298dbd9b54b45de1d35f7dd
Carlos Leonardao <74982565+ItsMeLeonardo@users.noreply.github.com>
2021-11-26 09:52:45 -0500

    first commit - add eslint

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header'
+import Notes from './components/Notes'
+import AddNote from './components/AddNote'
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <AddNote />
+      <Notes />
+    </div>
+  )
+}
+
+export default App
